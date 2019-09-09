fb2e99703ac7524be0b0c52f8f7ff191b3cd2ba4
John Mateer <jj.mateer@live.com>
2019-09-08 22:48:03 -0700

    In the friends, I named all of the values the same as the values in the getScores variable in survey.html. In survey.html, I fixed a few of the form inputs where I accidentally put the id names in the type labels. In apiRoutes.js, I gave the require variable a unique name and used it in the post function. The function now successfully adds a new person to friendsArray.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,13 @@
 
-var friends = [
-    { "name": "Ahmed", "photo": "a", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] },
-    { "name": "Jacob Deming", "photo": "b", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] },
-    { "name": "Jeremiah Scanlon", "photo": "c", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] },
-    { "name": "Louis T. Delia", "photo":"d", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] },
-    { "name": "Lou Ritter", "photo": "e", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] },
-    { "name": "Jordan Biason", "photo": "f", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] },
-    { "name": "Isaac", "photo": "b", "scores": ["2", "3", "4", "2", "2", "2", "2", "3", "3", "3"] },
-    { "name": "Pedro", "photo": "h", "scores": ["4", "3", "4", "4", "4", "2", "3", "3", "3", "1"] }
+var friendsArray = [
+    { name: "Ahmed", picture: "a", q1: "5", q2: "1", q3: "4", q4: "4", q5: "5", q6: "1", q7: "2", q8: "5", q9: "4", q10: "1" },
+    { name: "Jacob Deming", picture: "b", q1: "4", q2: "2", q3: "5", q4: "1", q5: "3", q6: "2", q7: "2", q8: "1", q9: "3", q10: "2" },
+    { name: "Jeremiah Scanlon", picture: "c", q1: "5", q2: "2", q3: "2", q4: "2", q5: "4", q6: "1", q7: "3", q8: "2", q9: "5", q10: "5" },
+    { name: "Louis T. Delia", picture: "d", q1: "4", q2: "4", q3: "2", q4: "3", q5: "2", q6: "2", q7: "3", q8: "2", q9: "4", q10: "5" },
+    { name: "Lou Ritter", picture: "e", q1: "4", q2: "3", q3: "4", q4: "1", q5: "5", q6: "2", q7: "5", q8: "3", q9: "1", q10: "4" },
+    { name: "Jordan Biason", picture: "f", q1: "4", q2: "4", q3: "2", q4: "3", q5: "2", q6: "2", q7: "3", q8: "2", q9: "4", q10: "5" },
+    { name: "Isaac", picture: "b", q1: "2", q2: "3", q3: "4", q4: "2", q5: "2", q6: "2", q7: "2", q8: "3", q9: "3", q10: "3" },
+    { name: "Pedro", picture: "h", q1: "4", q2: "3", q3: "4", q4: "4", q5: "4", q6: "2", q7: "3", q8: "3", q9: "3", q10: "1" }
 ]
 
-module.exports = friends;
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,15 @@
-var friends = require('../data/friends.js');
+var friendsData = require('../data/friends.js');
 
 module.exports = (app) => {
     app.get('/api/friends', (req, res) => {
-        res.json(friends);
+        res.json(friendsData);
     });
     app.post('/api/friends', (req, res) => {
-        if (friends.length > 10) {
-            friends.push(req.body);
+        if (friendsData.length > 10) {
+            friendsData.push(req.body);
             res.json(true);
         } else {
-            friends.push(req.body);
+            friendsData.push(req.body);
             res.json(false);
         }
     })
