01c0b031e5a619f4a66af6b23a9f28f91f72ddda
John Mateer <jj.mateer@live.com>
2019-09-10 03:29:17 -0700

    In apiRoutes, I added a for loop to iterate through the friends scores and filter out any numbers that do not match. The new array is stores in the diff variable, which I console logged along with the other arrays.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,14 +5,26 @@ module.exports = (app) => {
         res.json(friendsData);
     });
     app.post('/api/friends', (req, res) => {
-        if (friendsData.length < 20) {
+        if (friendsData.length < 11) {
             friendsData.push(req.body);
             res.json(true);
         } else {
             friendsData.push(req.body);
             res.json(false);
         }
-        console.log(req.body)
-        // friendsData.push(req.body);
-    })
+        // console.log(req.body.score)
+        for (var i = 0; i < friendsData.length; i++) {
+            // console.log(friendsData[i].score)
+            var diff = friendsData[i].score.filter(x => !req.body.score.includes(x));
+            console.log("==================")
+            console.log("==================")
+            console.log("user score: " + req.body.score)
+            console.log("==================")
+            console.log("friends scores: " + friendsData[i].score)
+            console.log("==================")
+            console.log("difference: " + diff)
+        }
+
+        // console.log(diff);
+    });
 }
