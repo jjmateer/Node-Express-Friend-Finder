5fa329bacba9b437eaba41a1e3580cc1b485fae0
John Mateer <jj.mateer@live.com>
2019-09-09 18:24:23 -0700

    Instead of individually pushing the values of the questions to scoreArr, I directly put them inside the array variable. In server.js, I had to change extended to true for the body parser to make sure the format of the score value is correctly formatted when being pushed to the friends array.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express')
 // var path = require('path');
 var app = express()
 var PORT = process.env.PORT || 8080
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 require('./app/routing/apiroutes.js')(app);
