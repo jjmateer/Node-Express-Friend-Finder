da58928d32e9ec8f6d8a3ea16b6a6d16c47c9683
John Mateer <jj.mateer@live.com>
2019-09-09 18:17:52 -0700

    Send to instructors

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,12 +5,14 @@ module.exports = (app) => {
         res.json(friendsData);
     });
     app.post('/api/friends', (req, res) => {
-        if (friendsData.length > 10) {
+        if (friendsData.length < 20) {
             friendsData.push(req.body);
             res.json(true);
         } else {
             friendsData.push(req.body);
             res.json(false);
         }
+        console.log(req.body)
+        // friendsData.push(req.body);
     })
 }
