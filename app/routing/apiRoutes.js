b594352e7a77f6125ae9857828d3848767d98dc1
John Mateer <jj.mateer@live.com>
2019-09-10 17:39:05 -0700

    Changed some things in the getTotal function so it creates a total variable which is equal to 0, uses a for loop to iterate through each diff array and add the total to each number in the diff array, which I had to convert into integers.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,34 +8,34 @@ module.exports = (app) => {
         if (friendsData.length < 11) {
             friendsData.push(req.body);
             res.json(true);
+            getArrs()
         } else {
             friendsData.push(req.body);
             res.json(false);
         }
         // console.log(req.body.score)
-        for (var i = 0; i < friendsData.length; i++) {
-            // console.log(friendsData[i].score)
-            var diff = friendsData[i].score.filter(x => !req.body.score.includes(x));
-            console.log("                  ")
-            console.log("                  ")
-            console.log("user score: " + req.body.score)
-            console.log("==================")
-            console.log("friends scores: " + friendsData[i].score)
-            console.log("==================")
-            console.log("difference: " + diff)
-            // console.log(
-            //     diff.reduce((a, b) => a + b, 0)
-            // )
-            getTotal()
-        }
-        function getTotal() {
-            // var totalDiff = 0;
-            for (var i = 0; i < diff.length; i++) {
-                var totalDiff = diff[i] + diff[i+1]
+        function getArrs() {
+            for (var i = 0; i < friendsData.length; i++) {
+                // console.log(friendsData[i].score)
+                var diff = friendsData[i].score.filter(x => !req.body.score.includes(x));
+                // console.log("                  ")
+                // console.log("                  ")
+                // console.log("user score: " + req.body.score)
+                // console.log("==================")
+                // console.log("friends scores: " + friendsData[i].score)
+                // console.log("==================")
+                console.log(diff)
+                getTotal()
+            }
+
+            function getTotal() {
+                var total = 0;
                 console.log("==================")
-                console.log("Total difference " + totalDiff)
+                for (var i = 0; i < diff.length; i++) {
+                    total += parseInt(diff[i])
+                }
+                console.log(total)
             }
         }
-
     });
 }
