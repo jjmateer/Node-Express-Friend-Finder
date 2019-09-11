93bfed4d5257581415e12d556dd6cbc9b86ebaab
John Mateer <jj.mateer@live.com>
2019-09-10 20:32:42 -0700

    Added a couple functions to apiRoutes which i am planning to get the totals of the friends scores and compare to the smallest number. Not functioning yet.this is a checkpoint

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,34 +8,67 @@ module.exports = (app) => {
         if (friendsData.length < 11) {
             friendsData.push(req.body);
             res.json(true);
-            getArrs()
+            getSmallestDiff()
+            // getArrTotals()
         } else {
             friendsData.push(req.body);
             res.json(false);
         }
         // console.log(req.body.score)
-        function getArrs() {
+        // function getArrTotals() {
+
+        //     const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        //     for (var i = 0; i < friendsData.length; i++) {
+        //         console.log(parseInt(req.body.score))
+
+        //         // console.log(parseInt(friendsData[i].score))
+        //     }
+
+        // }
+        function getSmallestDiff() {
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            var totalsArr = [];
+            var compareArr = [];
+            var isComplete = 0;
             for (var i = 0; i < friendsData.length; i++) {
-                // console.log(friendsData[i].score)
                 var diff = friendsData[i].score.filter(x => !req.body.score.includes(x));
-                // console.log("                  ")
-                // console.log("                  ")
-                // console.log("user score: " + req.body.score)
-                // console.log("==================")
-                // console.log("friends scores: " + friendsData[i].score)
-                // console.log("==================")
-                console.log(diff)
-                getTotal()
+                var total = 0;
+                for (var j = 0; j < diff.length; j++) {
+                    total += parseInt(diff[j])
+                }
+                totalsArr.push(total)
+                totalsArr.sort(function (a, b) { return a - b });
+                buildCompareTotals()
+            }
+            // console.log(lowestNum)
+            var lowestNum = totalsArr[1]
+            if (parseInt(req.body.score.reduce(reducer)) === lowestNum) {
+                console.log('yes')
+            }
+            function buildCompareTotals() {
+                const reducer = (accumulator, currentValue) => accumulator + currentValue;
+                isComplete++;
+                compareArr.push(parseInt(friendsData[i].score))
+                if (isComplete === friendsData.length) {
+                    // compareTotals()
+                    console.log(compareArr.reduce(reducer))
+                }
+            }
+            function compareTotals() {
+                const reducer = (accumulator, currentValue) => accumulator + currentValue;
+                console.log(friendsData.score)
+                var totalCompare = friendsData.score.reduce(reducer)
+                for (var i = 0; i < friendsData.length; i++) {
+                    console.log(totalCompare)
+                }
+
             }
 
-            function getTotal() {
-                var total = 0;
-                console.log("==================")
-                for (var i = 0; i < diff.length; i++) {
-                    total += parseInt(diff[i])
-                }
-                console.log(total)
-            }
+
+
+            
         }
-    });
+
+    })
+    // console.log(friendsData[i].score)
 }
