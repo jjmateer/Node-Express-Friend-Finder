b821c10264bfc745e53979be7af34157ffc05780
John Mateer <jj.mateer@live.com>
2019-09-11 14:29:53 -0700

    Created a new function in apiRoutes that does not add the user to directly to the friends list and compares the scores one by one to the friends scores. The function gets the difference by subtracting the user score from the friend score and getting the absolute value from the total score. After that, set the bestmatch name, picture, and difference equal to the name and picture of the current friend & the difference to the difference between the users score and the current friend's. Sent a json response containing the best match and used the ajax post function in survey.html to append the data from the best matching friend to a newly created div which I placed underneath the submit button.

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,7 +7,8 @@ var friendsArray = [
     { name: "Lou Ritter", picture: "e", score: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] },
     { name: "Jordan Biason", picture: "f", score: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] },
     { name: "Isaac", picture: "b", score: ["2", "3", "4", "2", "2", "2", "2", "3", "3", "3"] },
-    { name: "Pedro", picture: "h", score: ["4", "3", "4", "4", "4", "2", "3", "3", "3", "1"] }
+    { name: "Pedro", picture: "h", score: ["4", "3", "4", "4", "4", "2", "3", "3", "3", "1"] },
+    { name: "dude", picture: "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555925477/shape/mentalfloss/92433188.jpg", score: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"] },
 ]
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,16 +4,19 @@ module.exports = (app) => {
     app.get('/api/friends', (req, res) => {
         res.json(friendsData);
     });
+    var match = "";
     app.post('/api/friends', (req, res) => {
-        if (friendsData.length < 11) {
-            friendsData.push(req.body);
-            res.json(true);
-            getSmallestDiff()
-            // getArrTotals()
-        } else {
-            friendsData.push(req.body);
-            res.json(false);
-        }
+        // if (friendsData.length < 11) {
+        //     friendsData.push(req.body);
+        //     // res.json(true);
+        //     getSmallestDiff()
+        //     res.json(match)
+        // } else {
+        //     friendsData.push(req.body);
+        //     // res.json(false);
+        //     res.json(match)
+        // }
+        // getSmallestDiff()
         function getSmallestDiff() {
             const reducer = (accumulator, currentValue) => accumulator + currentValue;
             var totalsArr = [];
@@ -30,9 +33,37 @@ module.exports = (app) => {
             var sortedArr = totalsArr.slice().sort(function (a, b) { return a - b });
             var lowestNum = sortedArr[1]
             var numIndex = totalsArr.indexOf(lowestNum);
-            console.log(friendsData[numIndex])
-            console.log(sortedArr)
-            console.log(totalsArr)
+            match = friendsData[numIndex]
+            console.log(match)
+            // console.log(sortedArr)
+            // console.log(totalsArr)
+            // res.json(match)
+            res.json(match)
         }
+
+
+        function getLeastDiff() {
+            var bestmatch = {
+                name: "",
+                picture: "",
+                friendDiff: 1000
+            }
+            for (var i = 0; i < friendsData.length; i++) {
+                var currentfriend = friendsData[i]
+                var difference = 0;
+                for (var j = 0; j < currentfriend.score.length; j++) {
+                    var currentfriendscore = currentfriend.score[j]
+                    var userscore = req.body.score[j]
+                    difference += Math.abs(parseInt(currentfriendscore) - parseInt(userscore))
+                }
+                if (difference < bestmatch.friendDiff) {
+                    bestmatch.name = currentfriend.name
+                    bestmatch.picture = currentfriend.picture
+                    bestmatch.friendDiff = difference
+                }
+            }
+            res.json(bestmatch)
+        }
+        getLeastDiff()
     })
 }
