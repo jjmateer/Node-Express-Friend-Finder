455a8c42ebb34e67c16d80b3f36e5f66450c14b5
John Mateer <jj.mateer@live.com>
2019-09-12 15:16:31 -0700

    Used ajax post to append the name and picture of match to the modal and linked the correct version of jquery.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,44 +4,7 @@ module.exports = (app) => {
     app.get('/api/friends', (req, res) => {
         res.json(friendsData);
     });
-    var match = "";
     app.post('/api/friends', (req, res) => {
-        // if (friendsData.length < 11) {
-        //     friendsData.push(req.body);
-        //     // res.json(true);
-        //     getSmallestDiff()
-        //     res.json(match)
-        // } else {
-        //     friendsData.push(req.body);
-        //     // res.json(false);
-        //     res.json(match)
-        // }
-        // getSmallestDiff()
-        function getSmallestDiff() {
-            const reducer = (accumulator, currentValue) => accumulator + currentValue;
-            var totalsArr = [];
-            var compareArr = [];
-            var isComplete = 0;
-            for (var i = 0; i < friendsData.length; i++) {
-                var diff = friendsData[i].score.filter(x => !req.body.score.includes(x));
-                var total = 0;
-                for (var j = 0; j < diff.length; j++) {
-                    total += parseInt(diff[j])
-                }
-                totalsArr.push(total)
-            }
-            var sortedArr = totalsArr.slice().sort(function (a, b) { return a - b });
-            var lowestNum = sortedArr[1]
-            var numIndex = totalsArr.indexOf(lowestNum);
-            match = friendsData[numIndex]
-            console.log(match)
-            // console.log(sortedArr)
-            // console.log(totalsArr)
-            // res.json(match)
-            res.json(match)
-        }
-
-
         function getLeastDiff() {
             var bestmatch = {
                 name: "",
